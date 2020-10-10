cbe927c331894150174dfb531aad593061fe2e74
cnzbpy <2207191504@qq.com>
2020-10-10 21:16:23 +0800

    Create 进度条.py

---
+++ 进度条.py
@@ -0,0 +1,15 @@
+import time
+for i in range(1,101):
+    print('\r'+'▋'*i+'当前进度：{}%'.format(i),end='')
+    time.sleep(0.3)
+
+
+import time
+start=time.perf_counter()
+for i in range(1,101):
+    a='*'*i
+    b='.'*(100-i)
+    c=i
+    dur=time.perf_counter()-start
+    print('\r{}{}当前进度：{}%，用时{:.2f}s'.format(a,b,c,dur),end='')
+    time.sleep(0.3)
