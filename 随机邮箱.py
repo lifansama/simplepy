9d52c0df8c2fec129987edca1211cddcdb394a17
cnzbpy <2207191504@qq.com>
2020-10-17 09:28:37 +0800

    Update 随机邮箱.py

--- 随机邮箱.py
+++ 随机邮箱.py
@@ -1,8 +1,17 @@
+#生成任意个数、任意位数且互不重复的随机数：
 import random
-n=int(input('随机邮箱名的长度='))
-s='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-name=''
-for _ in range(n):
-    i=random.choice(s)
-    name=name+i
-print(name+'@qq.com')
+n1=int(input('随机数个数='))
+n2=int(input('随机数长度='))
+l=[]
+s='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'#这里可以加入你想要的字符
+while True:
+    name=''
+    for _ in range(n2):
+        i=random.choice(s)
+        name=name+i
+    if name not in l:
+        l.append(name)
+        if len(l)==n1:
+            for j in l:
+                print(j+'@qq.com')
+            break
